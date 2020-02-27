4ed8b3361fc2ea449a1aa79831d8d39bf8f99dc2
David Cantrell <david@cantrell.org.uk>
2020-02-27 23:37:15 +0000

    tests

---
+++ test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+STATUS=0
+
+function main {
+    ok "$(./makelink.sh)" "provide two arguments" "Called with no args, it whines"
+
+    ./makelink.sh foo bar
+    ok "$([ -f foo ];echo $?)" "0" "Created file 'foo'"
+    ok "$([ -L bar ];echo $?)" "0" "Created symlink 'bar'"
+}
+
+function ok {
+    local got=$1
+    local expected=$2
+    local message=$3
+    if [ "$1" != "$2" ]; then
+        echo "not ok $message"
+        STATUS=1
+    else
+        echo "ok $message"
+    fi
+}
+
+main
+rm foo bar
+exit $STATUS
